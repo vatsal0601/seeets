33b6fbaf69dbdf3664ddc470d59afffe65afd1f2
Vatsal Sakariya <vsakariya8@gmail.com>
2024-04-20 17:31:36 +0530

    created home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,29 @@ export const metadata = {
   description:
     "a free and open-source platform for tracking workouts and analyzing progress.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: ["workout", "tracker", "progress", "analysis"],
+  authors: [{ name: "Vatsal Sakariya", url: "https://twitter.com/vatsal0601" }],
+  openGraph: {
+    title: "seeets",
+    description:
+      "a free and open-source platform for tracking workouts and analyzing progress.",
+    url: "https://seeets.vercel.app",
+    type: "website",
+    locale: "en_US",
+    siteName: "seeets",
+    images: [{ url: "/og-image.png", alt: "seeets" }],
+  },
+  twitter: {
+    title: "seeets",
+    description:
+      "a free and open-source platform for tracking workouts and analyzing progress.",
+    creator: "@vatsal0601",
+    site: "@vatsal0601",
+    card: "summary_large_image",
+    images: [{ url: "/og-image.png", alt: "seeets" }],
+    creatorId: "vatsal0601",
+    siteId: "vatsal0601",
+  },
 };
 
 export default function RootLayout({
