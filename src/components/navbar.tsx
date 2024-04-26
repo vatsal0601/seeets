ce3cc87efb21cd8121e76580d582e4cb1f8cd2aa
Vatsal Sakariya <vsakariya8@gmail.com>
2024-04-26 19:11:04 +0530

    fix nav padding and button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,16 +6,21 @@ import { ThemeToggle } from "~/components/theme-toggle";
 function Navbar() {
   return (
     <nav className="fixed top-0 w-full bg-background/10 backdrop-blur-[2px]">
-      <div className="container flex h-16 items-center justify-between">
+      <div className="flex h-16 items-center justify-between px-5 lg:px-10">
         <h3 className="text-2xl font-extrabold tracking-tight lg:text-3xl">
           seeets
         </h3>
         <ul className="flex items-center gap-3">
           <li>
-            <Button size="sm" className="h-10" asChild>
+            <Button size="sm" className="hidden h-10 lg:inline-flex" asChild>
               <Link href="/interval-timer">
                 <Timer className="mr-1 size-[1.2rem]" />
                 <span>interval timer</span>
+              </Link>
+            </Button>
+            <Button size="icon" className="lg:hidden" asChild>
+              <Link href="/interval-timer">
+                <Timer className="size-[1.2rem]" />
               </Link>
             </Button>
           </li>
